28e4d0f1c4f14b7bce0c8300328e47549ee17b83
tonilin <tonilin@gmail.com>
2013-04-18 15:06:32 +0800

    add devise and binding to topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,11 +1,11 @@
 # -*- encoding : utf-8 -*-
 class TopicsController < ApplicationController
-  before_filter :find_board, :only => [:new, :create]
-  before_filter :find_topic, :only => [:show, :destroy, :edit, :update]
+  before_filter :authenticate_user!, :except => [:show]
+  before_filter :find_board
+  before_filter :find_topic, :except => [:new, :create]
+  before_filter :validate_owner, :only => [:edit, :update, :destroy,]
 
   def show
-    @board = @topic.board
-
 
     drop_breadcrumb(@board.title, board_path(@board))
     drop_breadcrumb(@topic.title)
@@ -15,13 +15,13 @@ class TopicsController < ApplicationController
   def new
     @topic = @board.topics.build
 
-
     drop_breadcrumb(@board.title, board_path(@board))
     drop_breadcrumb("新增主題")
   end
 
   def create
     @topic = @board.topics.build(params[:topic])
+    @topic.user = current_user
 
     if @topic.save
       redirect_to board_topic_path(@board ,@topic)
@@ -33,7 +33,6 @@ class TopicsController < ApplicationController
 
 
   def destroy
-    @board = @topic.board
     @topic.destroy
 
     redirect_to board_path(@board)
@@ -41,15 +40,12 @@ class TopicsController < ApplicationController
   end
 
   def edit
-    @board = @topic.board
-
     drop_breadcrumb(@board.title, board_path(@board))
     drop_breadcrumb(@topic.title, board_topic_path(@board,@topic))
     drop_breadcrumb("編輯主題")
   end
 
   def update
-    @board = @topic.board
 
     if @topic.update_attributes( params[:topic] )
       redirect_to board_topic_path(@board)
@@ -71,6 +67,12 @@ class TopicsController < ApplicationController
 
   def find_topic
     @topic = Topic.find(params[:id])
+  end
+
+  def validate_owner
+    if @topic.user != current_user
+      redirect_to board_topic_path(@board, @topic), :notice => "You are not the owner of the topic"
+    end
   end
 
 
