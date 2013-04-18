9857e6b4423ca9746cbfa0b2aaa64e37a2e81127
tonilin <tonilin@gmail.com>
2013-04-18 11:15:46 +0800

    add board view

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,21 @@
 class BoardsController < ApplicationController
+  before_filter :find_board, :only => [:show]
+
+  def show
+    @topics = @board.topics
+
+
+  end
+
+
+
+
+  protected
+
+  def find_board
+    @board = Board.find(params[:id])
+
+  end
+
+
 end
