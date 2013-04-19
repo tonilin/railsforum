d2d2ab70e538c72e55a5d64cfdc3cca1e8b2539e
tonilin <tonilin@gmail.com>
2013-04-19 11:01:17 +0800

    apply boostrappers

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-SITE_NAME = "RORFORUM"
-
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -13,7 +11,6 @@ end
 
 module Railsforum
   class Application < Rails::Application
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
